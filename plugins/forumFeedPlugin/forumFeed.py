061f2aa2cede30381536cf18e082544b5091b5de
Aurora0000 <aurora@int0x10.com>
2015-04-19 11:00:05 +0100

    Refactored forumFeed plugin

--- plugins/forumFeedPlugin/forumFeed.py
+++ plugins/forumFeedPlugin/forumFeed.py
@@ -4,8 +4,15 @@ import string
 import feedparser
 class forumFeed(IPlugin):
     def botmsg(self, user, channel, task, args):
+        if not args:
+            return  # All of our commands require arguments.
         manager = PluginManagerSingleton.get()
+        if channel == manager.app.nickname:
+            channel = user  # Crafty hack to allow bot to PM users if message
+                            # isn't in a channel.
         if task == manager.app.plugin_get_setting("forumFeed", "respondTo") and args[0] == "latest":
             feed = feedparser.parse(manager.app.plugin_get_setting("forumFeed", "feedUrl"))
             user = user.split("!")[0]   # Remove the user and host
-            manager.app.msg(channel, user + ": The latest message is \"" + str(feed.entries[0].title) + "\" at " + str(feed.entries[0].link))
+            title = str(feed.entries[0].title)
+            link = str(feed.entries[0].link)
+            manager.app.msg(channel, user + ": The latest message is \"" + title + "\" at " + link)
