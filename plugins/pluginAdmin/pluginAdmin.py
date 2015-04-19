524192379028684b641851cca1a2bb258262449f
Aurora0000 <aurora@int0x10.com>
2015-04-19 11:17:43 +0100

    Enhanced pluginAdmin to stop users from unloading it and not being able to reload

--- plugins/pluginAdmin/pluginAdmin.py
+++ plugins/pluginAdmin/pluginAdmin.py
@@ -14,9 +14,18 @@ class pluginAdmin(IPlugin):
                 manager.app.msg(channel, "Plugins rehashed!")
             elif args[0] == "load":
                 manager = PluginManagerSingleton.get()
-                manager.app.load_plugin(string.join(args[1:]))
-                manager.app.msg(channel, "Plugin" + string.join(args[1:]) + " has been loaded.")
+                pname = string.join(args[1:])
+                if pname in manager.app.plugin_get_setting("pluginAdmin", "disallowedPlugins"):
+                    manager.app.msg(channel, "Plugin \"" + pname + "\" is protected!")       
+                    return
+                manager.app.load_plugin(pname)
+                manager.app.msg(channel, "Plugin \"" + string.join(args[1:]) + "\" has been loaded.")
             elif args[0] == "unload":
                 manager = PluginManagerSingleton.get()
+                pname = string.join(args[1:])
+                if pname in manager.app.plugin_get_setting("pluginAdmin", "disallowedPlugins"):
+                    manager.app.msg(channel, "Plugin \"" + pname + "\" is protected!")       
+                    return
                 manager.app.unload_plugin(string.join(args[1:]))
-                manager.app.msg(channel, "Plugin" + string.join(args[1:]) + " has been unloaded.")
+                manager.app.msg(channel, "Plugin \"" + string.join(args[1:]) + "\" has been unloaded.")
+ 
