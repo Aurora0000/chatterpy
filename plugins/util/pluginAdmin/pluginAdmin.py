3ba35ce5905d497327aba2eaf57bffd503ae213c
Aurora0000 <Aurora0000@users.noreply.github.com>
2015-06-23 17:17:31 +0100

    Updated some plugins

--- plugins/util/pluginAdmin/pluginAdmin.py
+++ plugins/util/pluginAdmin/pluginAdmin.py
@@ -1,6 +1,7 @@
 from yapsy.IPlugin import IPlugin
 from yapsy.PluginManager import PluginManagerSingleton
 import string
+import itertools
 # The python file's name must be the same as the .chatter file's module attribute
 class pluginAdmin(IPlugin):
     def botmsg(self, user, channel, task, args):
@@ -16,16 +17,20 @@ class pluginAdmin(IPlugin):
                 manager = PluginManagerSingleton.get()
                 pname = string.join(args[1:])
                 if pname in manager.app.plugin_get_setting("pluginAdmin", "disallowedPlugins"):
-                    manager.app.msg(channel, "Plugin \"" + pname + "\" is protected!")       
+                    manager.app.msg(channel, "Plugin \"" + pname + "\" is protected!")      
                     return
                 manager.app.load_plugin(pname)
                 manager.app.msg(channel, "Plugin \"" + string.join(args[1:]) + "\" has been loaded.")
+            elif args[0] == "list":
+                manager = PluginManagerSingleton.get()
+                plugins_list = manager.getAllPlugins()
+                plugins_names = itertools.imap(lambda plugin: plugin.name, plugins_list)
+                manager.app.msg(channel, ' | '.join(plugins_names))
             elif args[0] == "unload":
                 manager = PluginManagerSingleton.get()
                 pname = string.join(args[1:])
                 if pname in manager.app.plugin_get_setting("pluginAdmin", "disallowedPlugins"):
-                    manager.app.msg(channel, "Plugin \"" + pname + "\" is protected!")       
+                    manager.app.msg(channel, "Plugin \"" + pname + "\" is protected!")      
                     return
                 manager.app.unload_plugin(string.join(args[1:]))
                 manager.app.msg(channel, "Plugin \"" + string.join(args[1:]) + "\" has been unloaded.")
- 
